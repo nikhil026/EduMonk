538e436885be6f8090ac6b3bcbe58e68a6b23abe
Nikhil Gupta <nikhil051097@gmail.com>
2017-08-20 21:35:37 +0530

    co

--- config/auth.js
+++ config/auth.js
@@ -11,16 +11,10 @@ module.exports = {
 
     },
 
-    'twitterAuth' : {
-        'consumerKey'        : 'your-consumer-key-here',
-        'consumerSecret'     : 'your-client-secret-here',
-        'callbackURL'        : 'http://localhost:8080/auth/twitter/callback'
-    },
-
     'googleAuth' : {
         'clientID'         : '842917659340-9m8bamcnndgdru6s1h610fp22ucki1un.apps.googleusercontent.com',
         'clientSecret'     : 'QTQ5ADogIisOMi4-uYX2mdqU',
-        'callbackURL'      : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'      : 'http://edumonk.herokuapp.com/auth/google/callback'
     }
 
 };
