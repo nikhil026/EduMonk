facdd1ab6c32c39f053d0374c7465300e4efacb6
Nikhil Gupta <nikhil051097@gmail.com>
2017-08-20 21:44:45 +0530

    commit

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '772186582987973', // your App ID
         'clientSecret'    : 'bb6d832834e87a4296d186c5c58bc62b', // your App Secret
-        'callbackURL'     : 'http://localhost:8080/auth/facebook/callback',
+        'callbackURL'     : 'http://edumonk.herokuapp.com/auth/facebook/callback',
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email,photos'
 
     },
